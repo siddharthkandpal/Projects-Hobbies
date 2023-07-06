7732a069f031993b5897c4761ccc02374c1b0ea0
Siddharth Kandpal <123426199+siddharthkandpal@users.noreply.github.com>
2023-07-05 21:51:35 -0400

    Update uart_receiver.v

--- UART_Verilog/uart_receiver.v
+++ UART_Verilog/uart_receiver.v
@@ -19,5 +19,23 @@ module uart_reciever#(
   );
 
   // state machines
-  localparam IDLE = 3'b000,
-            
+  localparam IDLE  =  3'b000, // if tx_start and !rst , move to state start and so on..
+             START =  3'b001, 
+             DATA  =  3'b010,
+             PARITY = 3'b011,
+             STOP  =  3'b100;            
+  reg[2:0] state; // 3 bit register to hold 5 states
+  state = IDLE;
+
+  reg[WORD_LENGTH_ + 2:0] shift_register;
+  reg[3:0] rx_bit_cnt;
+  reg[3:0] stop_bit_cnt;
+
+  // as both the devices are operating at the same baud rate, we get using the same formula: CLKFREQ/BAUDRATE - 1
+
+  localparam MAX_COUNT = CLK_FREQ / BAUD_RATE / 2 - 1;
+  reg [31:0] counter = 0;
+  reg baud_tick = 0;
+  
+  
+  
