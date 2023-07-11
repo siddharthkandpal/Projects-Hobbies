414385b491a0d0e1bcc6c7f080fc680c939e05da
Siddharth Kandpal <123426199+siddharthkandpal@users.noreply.github.com>
2023-07-11 13:02:24 -0400

    Update Mealy_sequence_detector_0111_tb.v

--- MINI_MODULES/Mealy_sequence_detector_0111_tb.v
+++ MINI_MODULES/Mealy_sequence_detector_0111_tb.v
@@ -1,16 +1,43 @@
 // testbench file for mealy 0111
 
 module mealy tb;
-  reg clk
+  reg clk;
   reg in;
   reg rst;
   reg out;
 
   //port instantiation
 
-  Mealy_FSM 
+  mealy_fsm UUT (
+    .clk(clk),
+    .rst(rst),
+    .in(in),
+    .out(out));
+
+
+  //clock settings
+  
+  initial begin
+    clk = 0;
+  forever clk = ~clk;
+  end
 
   initial begin
-    #5 clk = ~clk;
+    in = 0;
+    rst = 1;
+
+    #20;
+    rst = 1;
+    #40;
+    in = 1;
+    #50;
+    in = 1;
+    #60;
+    in = 1;
+    #70;
+    in = 0;
+    #80;
+    in = 1;
+
   end
-  
+endmodule
